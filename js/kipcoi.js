b3a01794a665fce32509438c4b97451b4a965ed5
ldebrot <ldebrot@yahoo.de>
2017-03-28 01:23:28 +0200

    added resize, purified HTML

--- js/kipcoi.js
+++ js/kipcoi.js
@@ -1,483 +1,128 @@
-/**
- * A Grid is the model of the playfield containing hexes
- * @constructor
+(function(){
+/* SOURCES 
+ *  https://github.com/kittykatattack/learningPixi/blob/master/README.md#installingpixithesimpleway
+ * 
+ * 
  */
-
-var HT = {};
-
-HT.Grid = function(/*double*/ width, /*double*/ height) {
 	
-	this.Hexes = [];
-	//setup a dictionary for use later for assigning the X or Y CoOrd (depending on Orientation)
-	var HexagonsByXOrYCoOrd = {}; //Dictionary<int, List<Hexagon>>
+	
+	console.log("KIPCOI - off we go!");
 
-	var row = 0;
-	var y = 0.0;
-	while (y + HT.Hexagon.Static.HEIGHT <= height)
-	{
-		var col = 0;
+/************************************ DEFAULT SETUP *********************************/
+window.ldb = {};
+window.ldb.x = 800;
+window.ldb.y = 800;
+window.ldb.pixioptions = {antialias: false, transparent: false, resolution: 1};
 
-		var offset = 0.0;
-		if (row % 2 == 1)
-		{
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				offset = (HT.Hexagon.Static.WIDTH - HT.Hexagon.Static.SIDE)/2 + HT.Hexagon.Static.SIDE;
-			else
-				offset = HT.Hexagon.Static.WIDTH / 2;
-			col = 1;
-		}
-		
-		var x = offset;
-		while (x + HT.Hexagon.Static.WIDTH <= width)
-		{
-		    var hexId = this.GetHexId(row, col);
-			var h = new HT.Hexagon(hexId, x, y);
-			
-			var pathCoOrd = col;
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				h.PathCoOrdX = col;//the column is the x coordinate of the hex, for the y coordinate we need to get more fancy
-			else {
-				h.PathCoOrdY = row;
-				pathCoOrd = row;
-			}
-			
-			this.Hexes.push(h);
-			
-			if (!HexagonsByXOrYCoOrd[pathCoOrd])
-				HexagonsByXOrYCoOrd[pathCoOrd] = [];
-			HexagonsByXOrYCoOrd[pathCoOrd].push(h);
+/************************************ ON WINDOW LOAD *********************************/
+window.onload = function () {
+	//Check if WebGL supported
+    var type = "WebGL";
+    if(!PIXI.utils.isWebGLSupported()){
+	      type = "canvas";
+    }
+    PIXI.utils.sayHello(type);
 
-			col+=2;
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				x += HT.Hexagon.Static.WIDTH + HT.Hexagon.Static.SIDE;
-			else
-				x += HT.Hexagon.Static.WIDTH;
-		}
-		row++;
-		if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-			y += HT.Hexagon.Static.HEIGHT / 2;
-		else
-			y += (HT.Hexagon.Static.HEIGHT - HT.Hexagon.Static.SIDE)/2 + HT.Hexagon.Static.SIDE;
-	}
-
-	//finally go through our list of hexagons by their x co-ordinate to assign the y co-ordinate
-	for (var coOrd1 in HexagonsByXOrYCoOrd)
-	{
-		var hexagonsByXOrY = HexagonsByXOrYCoOrd[coOrd1];
-		var coOrd2 = Math.floor(coOrd1 / 2) + (coOrd1 % 2);
-		for (var i in hexagonsByXOrY)
-		{
-			var h = hexagonsByXOrY[i];//Hexagon
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				h.PathCoOrdY = coOrd2++;
-			else
-				h.PathCoOrdX = coOrd2++;
-		}
-	}
+	//Create the renderer
+	window.renderer = PIXI.autoDetectRenderer(ldb.x, ldb.y,ldb.pixioptions);
+	
+	//Add the canvas to the HTML document
+	document.getElementById('gamefield').appendChild(renderer.view);
+	
+	//Create a container object called the `stage`
+	var stage = new PIXI.Container();
+	
+	//Tell the `renderer` to `render` the `stage`
+	renderer.render(stage);
+	
 };
 
-HT.Grid.Static = {Letters:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'};
+/************************************ RESIZE *********************************/
 
-HT.Grid.prototype.GetHexId = function(row, col) {
-	var letterIndex = row;
-	var letters = "";
-	while(letterIndex > 25)
-	{
-		letters = HT.Grid.Static.Letters[letterIndex%26] + letters;
-		letterIndex -= 26;
-	}
-		
-	return HT.Grid.Static.Letters[letterIndex] + letters + (col + 1);
-};
+window.addEventListener("resize", function(event){ 
+  //scaleToWindow(renderer.view,"#2C3539");
+});
 
-/**
- * Returns a hex at a given point
- * @this {HT.Grid}
- * @return {HT.Hexagon}
- */
-HT.Grid.prototype.GetHexAt = function(/*Point*/ p) {
-	//find the hex that contains this point
-	for (var h in this.Hexes)
-	{
-		if (this.Hexes[h].Contains(p))
-		{
-			return this.Hexes[h];
-		}
-	}
+function scaleToWindow(canvas, backgroundColor) {
 
-	return null;
-};
+  backgroundColor = backgroundColor || "#2C3539";
+  var scaleX, scaleY, scale, center;
+  
+  //1. Scale the canvas to the correct size
+  //Figure out the scale amount on each axis
+  scaleX = window.innerWidth / canvas.offsetWidth;
+  scaleY = window.innerHeight / canvas.offsetHeight;
 
-/**
- * Returns a distance between two hexes
- * @this {HT.Grid}
- * @return {number}
- */
-HT.Grid.prototype.GetHexDistance = function(/*Hexagon*/ h1, /*Hexagon*/ h2) {
-	//a good explanation of this calc can be found here:
-	//http://playtechs.blogspot.com/2007/04/hex-grids.html
-	var deltaX = h1.PathCoOrdX - h2.PathCoOrdX;
-	var deltaY = h1.PathCoOrdY - h2.PathCoOrdY;
-	return ((Math.abs(deltaX) + Math.abs(deltaY) + Math.abs(deltaX - deltaY)) / 2);
-};
+  //Scale the canvas based on whichever value is less: `scaleX` or `scaleY`
+  scale = Math.min(scaleX, scaleY);
+  canvas.style.transformOrigin = "0 0";
+  canvas.style.transform = "scale(" + scale + ")";
+  console.log(scaleX);
 
-/**
- * Returns a distance between two hexes
- * @this {HT.Grid}
- * @return {HT.Hexagon}
- */
-HT.Grid.prototype.GetHexById = function(id) {
-	for(var i in this.Hexes)
-	{
-		if(this.Hexes[i].Id == id)
-		{
-			return this.Hexes[i];
-		}
-	}
-	return null;
-};
+  //2. Center the canvas.
+  //Decide whether to center the canvas vertically or horizontally.
+  //Wide canvases should be centered vertically, and 
+  //square or tall canvases should be centered horizontally
+  if (canvas.offsetwidth > canvas.offsetHeight) {
+    if (canvas.offsetWidth * scale < window.innerWidth) {
+      center = "horizontally";
+    } else { 
+      center = "vertically";
+    }
+  } else {
+    if (canvas.offsetHeight * scale < window.innerHeight) {
+      center = "vertically";
+    } else { 
+      center = "horizontally";
+    }
+  }
+  
+  //Center horizontally (for square or tall canvases)
+  var margin;
+  if (center === "horizontally") {
+    margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
+    canvas.style.marginTop = 0;
+    canvas.style.marginBottom = 0;
+    canvas.style.marginLeft = margin + "px";
+    canvas.style.marginRight = margin + "px";
+  }
 
-function findHexWithSideLengthZAndRatio()
-{
-	var z = parseFloat(document.getElementById("sideLength").value);
-	var r = parseFloat(document.getElementById("whRatio").value);
-	
-	//solve quadratic
-	var r2 = Math.pow(r, 2);
-	var a = (1 + r2)/r2;
-	var b = z/r2;
-	var c = ((1-4.0*r2)/(4.0*r2)) * (Math.pow(z, 2));
-	
-	var x = (-b + Math.sqrt(Math.pow(b,2)-(4.0*a*c)))/(2.0*a);
-	
-	var y = ((2.0 * x) + z)/(2.0 * r);
-	
-	var contentDiv = document.getElementById("hexStatus");
+  //Center vertically (for wide canvases) 
+  if (center === "vertically") {
+    margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
+    canvas.style.marginTop = margin + "px";
+    canvas.style.marginBottom = margin + "px";
+    canvas.style.marginLeft = 0;
+    canvas.style.marginRight = 0;
+  }
 
-	var width = ((2.0*x)+z);
-	var height = (2.0*y);
-	contentDiv.innerHTML = "Values for Hex: <br /><b>Side Length, z:</b> " + z + "<br /><b>x:</b> " + x + "<br /><b>y:</b> " + y +
-		"<br /><b>Width:</b> " + width + "<br /><b>Height: </b>" + height;
-	
-	HT.Hexagon.Static.WIDTH = width;
-	HT.Hexagon.Static.HEIGHT = height;
-	HT.Hexagon.Static.SIDE = z;
+  //3. Remove any padding from the canvas  and body and set the canvas
+  //display style to "block"
+  canvas.style.paddingLeft = 0;
+  canvas.style.paddingRight = 0;
+  canvas.style.paddingTop = 0;
+  canvas.style.paddingBottom = 0;
+  canvas.style.display = "block";
+  
+  //4. Set the color of the HTML body background
+  document.body.style.backgroundColor = backgroundColor;
+  
+  //Fix some quirkiness in scaling for Safari
+  var ua = navigator.userAgent.toLowerCase(); 
+  if (ua.indexOf("safari") != -1) { 
+    if (ua.indexOf("chrome") > -1) {
+      // Chrome
+    } else {
+      // Safari
+      //canvas.style.maxHeight = "100%";
+      //canvas.style.minHeight = "100%";
+    }
+  }
+
+  //5. Return the `scale` value. This is important, because you'll nee this value 
+  //for correct hit testing between the pointer and sprites
+  return scale;
 }
 
-function findHexWithWidthAndHeight()
-{
-	var width = parseFloat(document.getElementById("hexWidth").value);
-	var height = parseFloat(document.getElementById("hexHeight").value);
-	
-	
-	var y = height/2.0;
-	
-	//solve quadratic
-	var a = -3.0;
-	var b = (-2.0 * width);
-	var c = (Math.pow(width, 2)) + (Math.pow(height, 2));
-	
-	var z = (-b - Math.sqrt(Math.pow(b,2)-(4.0*a*c)))/(2.0*a);
-	
-	var x = (width - z)/2.0;
-	
-	var contentDiv = document.getElementById("hexStatus");
 
-	contentDiv.innerHTML = "Values for Hex: <br /><b>Width:</b> " + width + "<br /><b>Height: </b>" + height +
-		"<br /><b>Side Length, z:</b> " + z + "<br /><b>x:</b> " + x + "<br /><b>y:</b> " + y;
-	
-	HT.Hexagon.Static.WIDTH = width;
-	HT.Hexagon.Static.HEIGHT = height;
-	HT.Hexagon.Static.SIDE = z;
-}
-
-function drawHexGrid()
-{
-	var grid = new HT.Grid(800, 600);
-	var canvas = document.getElementById("hexCanvas");
-	var ctx = canvas.getContext('2d');
-	ctx.clearRect(0, 0, 800, 600);
-	for(var h in grid.Hexes)
-	{
-		grid.Hexes[h].draw(ctx);
-	}
-}
-
-function getHexGridZR()
-{
-	findHexWithSideLengthZAndRatio();
-	drawHexGrid();
-}
-
-function getHexGridWH()
-{
-	findHexWithWidthAndHeight();
-	drawHexGrid();
-}
-
-function changeOrientation()
-{
-	if(document.getElementById("hexOrientationNormal").checked)
-	{
-		HT.Hexagon.Static.ORIENTATION = HT.Hexagon.Orientation.Normal;
-	}
-	else
-	{
-		HT.Hexagon.Static.ORIENTATION = HT.Hexagon.Orientation.Rotated;
-	}
-	drawHexGrid();
-}
-
-function debugHexZR()
-{
-	findHexWithSideLengthZAndRatio();
-	addHexToCanvasAndDraw(20, 20);
-}
-
-function debugHexWH()
-{
-	findHexWithWidthAndHeight();
-	addHexToCanvasAndDraw(20, 20);
-}
-
-function addHexToCanvasAndDraw(x, y)
-{
-	HT.Hexagon.Static.DRAWSTATS = true;
-	var hex = new HT.Hexagon(null, x, y);
-	
-	var canvas = document.getElementById("hexCanvas");
-	var ctx = canvas.getContext('2d');
-	ctx.clearRect(0, 0, 800, 600);
-	hex.draw(ctx);
-}
-
-var HT = HT || {};
-/**
- * A Point is simply x and y coordinates
- * @constructor
- */
-HT.Point = function(x, y) {
-	this.X = x;
-	this.Y = y;
-};
-
-/**
- * A Rectangle is x and y origin and width and height
- * @constructor
- */
-HT.Rectangle = function(x, y, width, height) {
-	this.X = x;
-	this.Y = y;
-	this.Width = width;
-	this.Height = height;
-};
-
-/**
- * A Line is x and y start and x and y end
- * @constructor
- */
-HT.Line = function(x1, y1, x2, y2) {
-	this.X1 = x1;
-	this.Y1 = y1;
-	this.X2 = x2;
-	this.Y2 = y2;
-};
-
-/**
- * A Hexagon is a 6 sided polygon, our hexes don't have to be symmetrical, i.e. ratio of width to height could be 4 to 3
- * @constructor
- */
-HT.Hexagon = function(id, x, y) {
-	this.Points = [];//Polygon Base
-	var x1 = null;
-	var y1 = null;
-	if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal) {
-		x1 = (HT.Hexagon.Static.WIDTH - HT.Hexagon.Static.SIDE)/2;
-		y1 = (HT.Hexagon.Static.HEIGHT / 2);
-		this.Points.push(new HT.Point(x1 + x, y));
-		this.Points.push(new HT.Point(x1 + HT.Hexagon.Static.SIDE + x, y));
-		this.Points.push(new HT.Point(HT.Hexagon.Static.WIDTH + x, y1 + y));
-		this.Points.push(new HT.Point(x1 + HT.Hexagon.Static.SIDE + x, HT.Hexagon.Static.HEIGHT + y));
-		this.Points.push(new HT.Point(x1 + x, HT.Hexagon.Static.HEIGHT + y));
-		this.Points.push(new HT.Point(x, y1 + y));
-	}
-	else {
-		x1 = (HT.Hexagon.Static.WIDTH / 2);
-		y1 = (HT.Hexagon.Static.HEIGHT - HT.Hexagon.Static.SIDE)/2;
-		this.Points.push(new HT.Point(x1 + x, y));
-		this.Points.push(new HT.Point(HT.Hexagon.Static.WIDTH + x, y1 + y));
-		this.Points.push(new HT.Point(HT.Hexagon.Static.WIDTH + x, y1 + HT.Hexagon.Static.SIDE + y));
-		this.Points.push(new HT.Point(x1 + x, HT.Hexagon.Static.HEIGHT + y));
-		this.Points.push(new HT.Point(x, y1 + HT.Hexagon.Static.SIDE + y));
-		this.Points.push(new HT.Point(x, y1 + y));
-	}
-	
-	this.Id = id;
-	
-	this.x = x;
-	this.y = y;
-	this.x1 = x1;
-	this.y1 = y1;
-	
-	this.TopLeftPoint = new HT.Point(this.x, this.y);
-	this.BottomRightPoint = new HT.Point(this.x + HT.Hexagon.Static.WIDTH, this.y + HT.Hexagon.Static.HEIGHT);
-	this.MidPoint = new HT.Point(this.x + (HT.Hexagon.Static.WIDTH / 2), this.y + (HT.Hexagon.Static.HEIGHT / 2));
-	
-	this.P1 = new HT.Point(x + x1, y + y1);
-	
-	this.selected = false;
-};
-	
-/**
- * draws this Hexagon to the canvas
- * @this {HT.Hexagon}
- */
-HT.Hexagon.prototype.draw = function(ctx) {
-
-	if(!this.selected)
-		ctx.strokeStyle = "grey";
-	else
-		ctx.strokeStyle = "black";
-	ctx.lineWidth = 1;
-	ctx.beginPath();
-	ctx.moveTo(this.Points[0].X, this.Points[0].Y);
-	for(var i = 1; i < this.Points.length; i++)
-	{
-		var p = this.Points[i];
-		ctx.lineTo(p.X, p.Y);
-	}
-	ctx.closePath();
-	ctx.stroke();
-	
-	if(this.Id)
-	{
-		//draw text for debugging
-		ctx.fillStyle = "black";
-		ctx.font = "bolder 8pt Trebuchet MS,Tahoma,Verdana,Arial,sans-serif";
-		ctx.textAlign = "center";
-		ctx.textBaseline = 'middle';
-		//var textWidth = ctx.measureText(this.Planet.BoundingHex.Id);
-		ctx.fillText(this.Id, this.MidPoint.X, this.MidPoint.Y);
-	}
-	
-	if(this.PathCoOrdX !== null && this.PathCoOrdY !== null && typeof(this.PathCoOrdX) != "undefined" && typeof(this.PathCoOrdY) != "undefined")
-	{
-		//draw co-ordinates for debugging
-		ctx.fillStyle = "black";
-		ctx.font = "bolder 8pt Trebuchet MS,Tahoma,Verdana,Arial,sans-serif";
-		ctx.textAlign = "center";
-		ctx.textBaseline = 'middle';
-		//var textWidth = ctx.measureText(this.Planet.BoundingHex.Id);
-		ctx.fillText("("+this.PathCoOrdX+","+this.PathCoOrdY+")", this.MidPoint.X, this.MidPoint.Y + 10);
-	}
-	
-	if(HT.Hexagon.Static.DRAWSTATS)
-	{
-		ctx.strokeStyle = "black";
-		ctx.lineWidth = 2;
-		//draw our x1, y1, and z
-		ctx.beginPath();
-		ctx.moveTo(this.P1.X, this.y);
-		ctx.lineTo(this.P1.X, this.P1.Y);
-		ctx.lineTo(this.x, this.P1.Y);
-		ctx.closePath();
-		ctx.stroke();
-		ctx.fillStyle = "black";
-		ctx.font = "bolder 8pt Trebuchet MS,Tahoma,Verdana,Arial,sans-serif";
-		ctx.textAlign = "left";
-		ctx.textBaseline = 'middle';
-		//var textWidth = ctx.measureText(this.Planet.BoundingHex.Id);
-		ctx.fillText("z", this.x + this.x1/2 - 8, this.y + this.y1/2);
-		ctx.fillText("x", this.x + this.x1/2, this.P1.Y + 10);
-		ctx.fillText("y", this.P1.X + 2, this.y + this.y1/2);
-		ctx.fillText("z = " + HT.Hexagon.Static.SIDE, this.P1.X, this.P1.Y + this.y1 + 10);
-		ctx.fillText("(" + this.x1.toFixed(2) + "," + this.y1.toFixed(2) + ")", this.P1.X, this.P1.Y + 10);
-	}
-};
-
-/**
- * Returns true if the x,y coordinates are inside this hexagon
- * @this {HT.Hexagon}
- * @return {boolean}
- */
-HT.Hexagon.prototype.isInBounds = function(x, y) {
-	return this.Contains(new HT.Point(x, y));
-};
-	
-
-/**
- * Returns true if the point is inside this hexagon, it is a quick contains
- * @this {HT.Hexagon}
- * @param {HT.Point} p the test point
- * @return {boolean}
- */
-HT.Hexagon.prototype.isInHexBounds = function(/*Point*/ p) {
-	if(this.TopLeftPoint.X < p.X && this.TopLeftPoint.Y < p.Y &&
-	   p.X < this.BottomRightPoint.X && p.Y < this.BottomRightPoint.Y)
-		return true;
-	return false;
-};
-
-//grabbed from:
-//http://www.developingfor.net/c-20/testing-to-see-if-a-point-is-within-a-polygon.html
-//and
-//http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html#The%20C%20Code
-/**
- * Returns true if the point is inside this hexagon, it first uses the quick isInHexBounds contains, then check the boundaries
- * @this {HT.Hexagon}
- * @param {HT.Point} p the test point
- * @return {boolean}
- */
-HT.Hexagon.prototype.Contains = function(/*Point*/ p) {
-	var isIn = false;
-	if (this.isInHexBounds(p))
-	{
-		//turn our absolute point into a relative point for comparing with the polygon's points
-		//var pRel = new HT.Point(p.X - this.x, p.Y - this.y);
-		var i, j = 0;
-		for (i = 0, j = this.Points.length - 1; i < this.Points.length; j = i++)
-		{
-			var iP = this.Points[i];
-			var jP = this.Points[j];
-			if (
-				(
-				 ((iP.Y <= p.Y) && (p.Y < jP.Y)) ||
-				 ((jP.Y <= p.Y) && (p.Y < iP.Y))
-				//((iP.Y > p.Y) != (jP.Y > p.Y))
-				) &&
-				(p.X < (jP.X - iP.X) * (p.Y - iP.Y) / (jP.Y - iP.Y) + iP.X)
-			   )
-			{
-				isIn = !isIn;
-			}
-		}
-	}
-	return isIn;
-};
-
-/**
-* Returns absolute distance in pixels from the mid point of this hex to the given point
-* Provided by: Ian (Disqus user: boingy)
-* @this {HT.Hexagon}
-* @param {HT.Point} p the test point
-* @return {number} the distance in pixels
-*/
-HT.Hexagon.prototype.distanceFromMidPoint = function(/*Point*/ p) {
-	// Pythagoras' Theorem: Square of hypotenuse = sum of squares of other two sides
-	var deltaX = this.MidPoint.X - p.X;
-	var deltaY = this.MidPoint.Y - p.Y;
-
-	// squaring so don't need to worry about square-rooting a negative number 
-	return Math.sqrt( (deltaX * deltaX) + (deltaY * deltaY) );
-};
-
-HT.Hexagon.Orientation = {
-	Normal: 0,
-	Rotated: 1
-};
-
-HT.Hexagon.Static = {HEIGHT:91.14378277661477
-					, WIDTH:91.14378277661477
-					, SIDE:50.0
-					, ORIENTATION:HT.Hexagon.Orientation.Normal
-					, DRAWSTATS: false};//hexagons will have 25 unit sides for now
+})();
